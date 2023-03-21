4ee5cc3bc4331b4b180750d5c2964a9641090fe0
Maximilliany <maxillian4@gmail.com>
2023-03-20 19:50:02 -0700

    Add method stubs, and constructor to ExtraLife.java.

--- src/main/java/org/bcit/comp2522/JaydenJump/ExtraLife.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/ExtraLife.java
@@ -1,4 +1,15 @@
 package org.bcit.comp2522.JaydenJump;
 
-public class ExtraLife {
+import processing.core.PImage;
+
+import java.awt.*;
+
+public class ExtraLife extends PowerUp{
+  public ExtraLife(int xpos, int ypos, int vx, int vy, int width, int height, boolean isActive, Color color, PImage image) {
+    super(xpos, ypos, vx, vy, width, height, isActive, color, image);
+  }
+
+  public void increaseLife() {
+
+  }
 }
