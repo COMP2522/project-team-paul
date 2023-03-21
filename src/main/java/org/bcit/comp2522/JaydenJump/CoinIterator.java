e3b9d6ff487f2f83967925a2d3ebd3af31b8f962
Maximilliany <maxillian4@gmail.com>
2023-03-20 19:13:15 -0700

    Add method stubs, instance variables, and constructor to CoinIterator.java.

--- src/main/java/org/bcit/comp2522/JaydenJump/CoinIterator.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/CoinIterator.java
@@ -1,4 +1,34 @@
 package org.bcit.comp2522.JaydenJump;
 
-public class CoinIterator {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class CoinIterator implements Iterator {
+
+  private ArrayList<Coin> coins;
+
+  public CoinIterator(ArrayList<Coin> coins) {
+    this.coins = coins;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public Object next() {
+    return null;
+  }
+
+  @Override
+  public void remove() {
+    Iterator.super.remove();
+  }
+
+  @Override
+  public void forEachRemaining(Consumer action) {
+    Iterator.super.forEachRemaining(action);
+  }
 }
