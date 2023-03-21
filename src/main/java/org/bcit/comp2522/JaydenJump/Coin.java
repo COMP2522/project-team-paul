feee0cd035e03c28e23a602bc0b48b03d0abcaa2
Maximilliany <maxillian4@gmail.com>
2023-03-20 19:00:19 -0700

    Add method stubs, instance variables, and constructor to Coin.java.

--- src/main/java/org/bcit/comp2522/JaydenJump/Coin.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/Coin.java
@@ -1,4 +1,59 @@
 package org.bcit.comp2522.JaydenJump;
 
 public class Coin {
+
+  private int value;
+
+  private int width;
+
+  private int height;
+
+  private String color;
+
+  public Coin(int value, int width, int height, String color) {
+    this.value = value;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  public void addToScore() {
+
+  }
+
+  public void playSound() {
+
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
 }
