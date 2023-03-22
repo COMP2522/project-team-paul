598a94110cf2f126a5b15816d1168bd7692310e4
Ravdeep Aulakh <vravdeep@gmail.com>
2023-03-22 15:20:04 -0700

    Added Player class methods and added getters and setters to sprite class

--- src/main/java/org/bcit/comp2522/JaydenJump/Sprite.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/Sprite.java
@@ -4,6 +4,39 @@ public abstract class Sprite implements Comparable<Object>, Drawable, Collideabl
   private float xpos;
   private float ypos;
   private float vx;
+
+  public float getXpos() {
+    return xpos;
+  }
+
+  public void setXpos(float xpos) {
+    this.xpos = xpos;
+  }
+
+  public float getYpos() {
+    return ypos;
+  }
+
+  public void setYpos(float ypos) {
+    this.ypos = ypos;
+  }
+
+  public float getVx() {
+    return vx;
+  }
+
+  public void setVx(float vx) {
+    this.vx = vx;
+  }
+
+  public float getVy() {
+    return vy;
+  }
+
+  public void setVy(float vy) {
+    this.vy = vy;
+  }
+
   private float vy;
 
   public Sprite(float xpos, float ypos, float vx, float vy) {
