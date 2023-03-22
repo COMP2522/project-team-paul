f7d4d9e018262f49ff4836856882c365f3b08580
Hyuk Park <hyukpk@hotmail.com>
2023-03-22 12:29:42 -0700

    Completed javadocs for PowerUp class

--- src/main/java/org/bcit/comp2522/JaydenJump/PowerUp.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/PowerUp.java
@@ -3,16 +3,57 @@ package org.bcit.comp2522.JaydenJump;
 import java.awt.Color;
 import processing.core.PImage;
 
-public abstract class PowerUp extends Sprite{
+/**
+ * The PowerUp class represents a type of consumable item that can be used by the player class.
+ * This class extends the Sprite class.
+ *
+ * @version 1.0
+ *
+ * @author Hyuk Park
+ *
+ * @since 2023-03-21
+ */
 
+public abstract class PowerUp extends Sprite {
+
+  /** The width of the PowerUp. */
   private int width;
+
+  /** The height of the PowerUp. */
   private int height;
+
+  /** State that determines whether the PowerUp is active or not. */
   private boolean isActive;
+
+  /**  The color of the PowerUp. */
   private Color color;
 
+  /** The image associated with the PowerUp. */
   private PImage image;
 
-  public PowerUp(int xpos, int ypos, int vx, int vy, int width, int height, boolean isActive, Color color, PImage image) {
+  /**
+   * Constructs the PowerUp class.
+   *
+   * @param xpos the x position of PowerUp
+   *
+   * @param ypos the y position of PowerUp
+   *
+   * @param vx the x velocity of PowerUp
+   *
+   * @param vy the y velocity of PowerUp
+   *
+   * @param width the width of PowerUp
+   *
+   * @param height the height of PowerUp
+   *
+   * @param isActive the state of PowerUp
+   *
+   * @param color the color of PowerUp
+   *
+   * @param image the image of PowerUp
+   */
+  public PowerUp(int xpos, int ypos, int vx, int vy, int width, int height,
+                 boolean isActive, Color color, PImage image) {
     super(xpos, ypos, vx, vy);
     this.width = width;
     this.height = height;
@@ -21,45 +62,100 @@ public abstract class PowerUp extends Sprite{
     this.image = image;
   }
 
+  /** Activates the PowerUp and affects the player class. */
   public void activate() {}
+
+  /** Deactivates the PowerUp. */
   public void deactivate() {}
 
+  /**
+   * Retrieves the width of the PowerUp.
+   *
+   * @return width of PowerUp
+   */
   public int getWidth() {
     return width;
   }
 
+  /**
+   * Reassigns the width of the PowerUp to a different size.
+   *
+   * @param width of PowerUp from user
+   */
   public void setWidth(int width) {
     this.width = width;
   }
 
+  /**
+   * Retrieves the height of the PowerUp.
+   *
+   * @return height of PowerUp
+   */
   public int getHeight() {
     return height;
   }
 
+  /**
+   * Reassigns height of the PowerUp to different size.
+   *
+   * @param height of PowerUp from user
+   */
   public void setHeight(int height) {
     this.height = height;
   }
 
+  /**
+   * Retrieves the boolean state of the PowerUp and checks if it is active (true)
+   * or not (false).
+   *
+   * @return boolean isActive of PowerUp
+   */
   public boolean isActive() {
     return isActive;
   }
 
+  /**
+   * Reassigns the isActive boolean of the PowerUp to either
+   * true (active) or false (in-active).
+   *
+   * @param active boolean from user
+   */
   public void setActive(boolean active) {
     isActive = active;
   }
 
+  /**
+   * Retrives the color of the PowerUp.
+   *
+   * @return color of PowerUp
+   */
   public Color getColor() {
     return color;
   }
 
+  /**
+   * Reassigns the color of the PowerUp to a new color.
+   *
+   * @param color from user
+   */
   public void setColor(Color color) {
     this.color = color;
   }
 
+  /**
+   * Retrieves the image of the PowerUp.
+   *
+   * @return image of PowerUp
+   */
   public PImage getImage() {
     return image;
   }
 
+  /**
+   * Reassigns the image of the PowerUp to a new image.
+   *
+   * @param image from user
+   */
   public void setImage(PImage image) {
     this.image = image;
   }
