d7e2f6573a9893d728be52e139efbdf1f9bff88f
Hyuk Park <hyukpk@hotmail.com>
2023-03-22 16:10:34 -0700

    Filled out javadocs for all powerup subtypes

--- src/main/java/org/bcit/comp2522/JaydenJump/ExtraLife.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/ExtraLife.java
@@ -4,11 +4,39 @@ import processing.core.PImage;
 
 import java.awt.*;
 
+/**
+ * A type of PowerUp that gives the player an extra life in the game.
+ *
+ * @version 1.0
+ *
+ * @author Shawn Birring
+ *
+ * @since 2023-03-22
+ */
+
 public class ExtraLife extends PowerUp{
-  public ExtraLife(int xpos, int ypos, int vx, int vy, int width, int height, boolean isActive, Color color, PImage image) {
+
+  /**
+   * Creates an instance of ExtraLife powerup in the game.
+   *
+   * @param xpos The x position of ExtraLife
+   * @param ypos The y position of ExtraLife
+   * @param vx The x velocity of ExtraLife
+   * @param vy The y velocity of ExtraLife
+   * @param width The width of ExtraLife
+   * @param height The height of ExtraLife
+   * @param isActive The boolean state that determines whether ExtraLife is active or not
+   * @param color The color of ExtraLife
+   * @param image The image of ExtraLife
+   */
+  public ExtraLife(int xpos, int ypos, int vx, int vy, int width, int height,
+                   boolean isActive, Color color, PImage image) {
     super(xpos, ypos, vx, vy, width, height, isActive, color, image);
   }
 
+  /**
+   * Increases the life of the Player by one.
+   */
   public void increaseLife() {
 
   }
--- src/main/java/org/bcit/comp2522/JaydenJump/JetPack.java
+++ src/main/java/org/bcit/comp2522/JaydenJump/JetPack.java
@@ -3,12 +3,39 @@ package org.bcit.comp2522.JaydenJump;
 import processing.core.PImage;
 import java.awt.Color;
 
-public class JetPack extends PowerUp{
+/**
+ * The JetPack class is a type of PowerUp that allows the Player to fly up
+ * screen for a certain period of time.
+ *
+ * @version 1.0
+ *
+ * @author Shawn Birring
+ *
+ * @since 2023-03-23
+ */
 
+public class JetPack extends PowerUp{
+  /** The amount of time the JetPack lasts. */
   private int duration;
 
+  /** The amount the y velocity of the player is increased. */
   private int boostVelocity;
 
+  /**
+   * Creates an Instance of a JetPack in the game.
+   *
+   * @param xpos The x position of JetPack
+   * @param ypos The y position of JetPack
+   * @param vx The x velocity of JetPack
+   * @param vy The y velocity of JetPack
+   * @param width The width of JetPack
+   * @param height The height of JetPack
+   * @param isActive The boolean state that determines whether JetPack is active or not
+   * @param color The color of JetPack
+   * @param image The image of JetPack
+   * @param duration The amount of time the JetPack lasts for
+   * @param boostVelocity The amount the y direction of Player is affected by JetPack
+   */
   public JetPack(int xpos, int ypos, int vx, int vy, int width, int height, boolean isActive, Color color, PImage image, int duration, int boostVelocity) {
     super(xpos, ypos, vx, vy, width, height, isActive, color, image);
     this.duration = duration;
