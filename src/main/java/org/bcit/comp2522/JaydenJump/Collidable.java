c9c3590d75f239c042d847f018dc274ca600911f
shawnbirring <shawnbirring21@gmail.com>
2023-04-06 18:49:58 -0700

    fixed typo and ran added javadocs to Drawable.java + Collideable.java

---
+++ src/main/java/org/bcit/comp2522/JaydenJump/Collidable.java
@@ -0,0 +1,17 @@
+package org.bcit.comp2522.JaydenJump;
+
+/**
+ * Interface for objects that can collide with other objects.
+ *
+ * @author Shawn Birring
+ */
+public interface Collidable {
+  /**
+   * Determines if this sprite collides with the specified object.
+   *
+   * @param o The object to check for collision
+   *
+   * @return True if the sprite collides with the object, false otherwise
+   */
+  boolean collides(Object o);
+}
